ebaface7f55e9de378199ac7664994671a123778
Your NameCpi2544 <pokpong1789@gmail.com>
2023-02-07 16:22:38 +0700

    lab16.cpp completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -6,14 +6,45 @@ using namespace std;
 void shuffle(int &,int &,int &,int &);
 
 int main(){
-	int a = 50, b = 100, c = 500, d = 1000;
-	
-	srand(time(0));	
-	
-	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d);
-	    cout << a << " " << b << " " << c << " " << d << "\n";
-	}
-	
-	return 0;
+ int a = 50, b = 100, c = 500, d = 1000;
+ 
+ srand(time(0)); 
+ 
+ for(int i = 0;i < 10;i++){
+     shuffle(a,b,c,d);
+     cout << a << " " << b << " " << c << " " << d << "\n";
+ }
+ 
+ return 0;
+}
+
+void shuffle(int &a,int &b,int &c,int &d)
+{
+    int temp, x[] = {a,b,c,d},y;
+    for(int i = 0, j =4; i < 4; i++, j--)
+    {
+        y = rand()%j;
+        if(y ==1)
+        {
+            temp = x[i];
+            x[i] = x[i+1];
+            x[i+1] = temp;
+        }
+        else if(y == 2)
+        {
+            temp = x[i];
+            x[i] = x[i+2];
+            x[i+2] = temp;
+        }
+        else if(y == 3)
+        {
+            temp = x[i];
+            x[i] = x[i+3];
+            x[i+3] = temp;
+        }
+    }
+    a = x[0];
+    b = x[1];
+    c = x[2];
+    d = x[3];
 }
